ecd57afb1f6bb1f4a1d48adc5d850788db0d335e
naomansaeed <naoman.saeed@gmail.com>
2025-11-21 07:10:59 +0500

    made my first 'api'

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,12 @@
 import express from "express"
 // const express = require("express")
+
 const app = express ()
+
+app.get("/api/notes", (req,res) => {
+    res.send("you got 5 notes");
+});
+
 app.listen(5001, () =>{
     console.log ("Server Started on PORT:5001");
-})
+});
