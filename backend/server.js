7025d7598a06123bed6d161e72e123fb0ad52665
naomansaeed <naoman.saeed@gmail.com>
2025-11-23 05:50:35 +0500

    added the routes for post & put.

--- backend/server.js
+++ backend/server.js
@@ -7,6 +7,18 @@ app.get("/api/notes", (req,res) => {
     res.status(200).send("you got 10 notes");
 });
 
+app.post("/api/notes", (req,res) => {
+    res.status(201).json({
+        message:"Post created Successfully!"
+    })
+})
+
+app.put("/api/notes", (req,res) => {
+    res.status(200).json({
+        message:"Post updated Successfully!"
+    })
+})
+
 app.listen(5001, () =>{
     console.log ("Server Started on PORT:5001");
 });
