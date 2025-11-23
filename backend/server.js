040d512a96b7f1e970691c263411c0f305c81ff5
naomansaeed <naoman.saeed@gmail.com>
2025-11-23 05:35:45 +0500

    shifted dev script to nodemon & added production script 'start'

--- backend/server.js
+++ backend/server.js
@@ -4,7 +4,7 @@ import express from "express"
 const app = express ()
 
 app.get("/api/notes", (req,res) => {
-    res.send("you got 5 notes");
+    res.status(200).send("you got 10 notes");
 });
 
 app.listen(5001, () =>{
