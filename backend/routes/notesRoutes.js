ffd40616c8ccec9a4f8242eac86ea3849afc03ee
naomansaeed <naoman.saeed@gmail.com>
2025-12-05 09:19:05 +0500

    rollback disfunctional changes.

--- backend/routes/notesRoutes.js
+++ backend/routes/notesRoutes.js
@@ -1,31 +1,27 @@
 import express from "express"
-import { createNote, deleteNote, getAllNotes, updateNote } from "../controllers/notesController";
+//import { createNote, deleteNote, getAllNotes, updateNote } from "../controllers/notesController";
 
 const router = express.Router();
 
-router.get("/", getAllNotes
-    //(req,res) => {
-    //res.status(200).send("you got 10 notes");}
-);
-
-router.post("/", createNote
-    //(req,res) => {
-    //res.status(201).json({
-    //    message:"Note created Successfully!"});}
+router.get("/", (req,res) => {
+    res.status(200).send("you got 10 notes");}
     );
 
-router.put("/:id", updateNote
-    //(req,res) => {
-    //res.status(200).json({
-    //    message:"Note updated Successfully!"
-    //});}
-);
+router.post("/", (req,res) => {
+    res.status(201).json({
+        message:"Note created Successfully!"});}
+    );
 
-router.delete("/:id", deleteNote
-    //(req,res) => {
-    //res.status(200).json({
-    //    message:"Note deleted Successfully!"
-    //});}
-);
+router.put("/:id", (req,res) => {
+    res.status(200).json({
+        message:"Note updated Successfully!"
+    });}
+    );
+
+router.delete("/:id", (req,res) => {
+    res.status(200).json({
+        message:"Note deleted Successfully!"
+    });}
+    );
 
 export default router
